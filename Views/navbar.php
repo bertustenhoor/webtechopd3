8a46a1338435f456acb9a6530e0e1731175b5baf
bertustenhoor <bertustenhoor@proton.me>
2024-11-13 15:55:48 +0100

    eerste layout items

---
+++ Views/navbar.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">ShareBoard</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Shares</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav me-0 mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+            </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Register</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- public/index.php
+++ public/index.php
@@ -14,9 +14,17 @@ include_once '../Controllers/UsersController.class.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <title>Home</title>
 </head>
 <body>
+<?php include_once '../Views/navbar.php' ?>
+<div class="container mt-5">
+    <h1 class="mb-3">Welcome to ShareBoard!</h1>
+    <h2 class="mb-3">Share your latest hot topics with the community</h2>
+    <div class="d-flex justify-content-center"><button class="btn btn-primary">Share now!</button></div>
+
   <?php
     $usersObj = new UsersView();
     $usersObj->showUser('Bertus');
@@ -24,5 +32,8 @@ include_once '../Controllers/UsersController.class.php';
     $usersObj2->createUser('No Giet', 'Sanders', '1999-07-07');
 
   ?>
+</div>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
 </html>
